17afcf75c005b86f38cb948ba72cef155d84efb2
quinncidences <qrc.inpa@gmail.com>
2019-05-24 10:09:15 -0700

    added clap hands with onclick to change

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import ClapFilled from './clapping-filled.png';
+import ClapUnfilled from './clapping-unfilled.png';
 
 class Post extends React.Component {
+
+  clapClick() {
+    let clap = document.getElementById("post-clap")
+    if (clap.alt === "Unfilled Clap") {
+      clap.src = ClapFilled
+      clap.alt = "Filled Clap"
+    } else {
+      clap.src = ClapUnfilled
+      clap.alt = "Unfilled Clap"
+    }
+
+  }
+
   render() {
   return (
-    <div className="post-container">
-      <p>POST AUTHOR</p>
-      <p>POST CONTENT</p>
-      <p>POST TIMESTAMP</p>
+    <div>
+      <div className="post-container">
+        <p>POST AUTHOR</p>
+        <p>POST CONTENT</p>
+        <p>POST TIMESTAMP</p>
+      </div>
+      <div className="clap-image">
+        <img id="post-clap" src={ClapUnfilled} alt="Unfied Clap" onClick={() => this.clapClick()}/>
+      </div>
     </div>
   )}
 }
